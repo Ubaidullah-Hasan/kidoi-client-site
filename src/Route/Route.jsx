181cc00093ed3f01e1f05e5e6c9b5f9703cd42eb
Ubaidullah-Hasan <hm2964133@gmail.com>
2023-05-20 01:25:17 +0600

    navbar add without condition

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,11 +1,14 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import Root from "../Layout/Root";
+import NotFound from "../Pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root></Root>,
+        errorElement: <NotFound></NotFound>,
     },
 ]);
 
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from "../../assets/logo.png"
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from "react-icons/fa";
+
+
+const Navbar = () => {
+
+    const menu = <>
+        <li className='cursor-pointer'> Home </li>
+        <li className='cursor-pointer'> All Toys</li>
+        <li className='cursor-pointer'> My Toys</li>
+        <li className='cursor-pointer'> Add A Toy</li>
+        <li className='cursor-pointer'> Blogs</li>
+    </>
+
+    return (
+        <div className='bg-orange-300'>
+            <div className="navbar md:w-[90%] mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-4 shadow bg-base-100  w-52 gap-5 left-[-8px]">
+                            {menu}
+                        </ul>
+                    </div>
+                    <Link to="/"><img src={logo} alt="LOGO" /></Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal gap-6 px-1">
+                        {menu}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <FaUserCircle className='w-6 h-6' />
+                    {/* <img src={} alt="" /> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
